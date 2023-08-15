a89cd3b55a7ef7ba9adfdc01748c473c7455ad98
Victoriya V. Forsythe <116663397+victoriyaforsythe@users.noreply.github.com>
2023-08-15 15:59:47 -0400

    Update __init__.py

--- PyIRI/__init__.py
+++ PyIRI/__init__.py
@@ -6,6 +6,9 @@ from PyIRI import igrf_library  # noqa F401
 from PyIRI import main_library  # noqa F401
 from PyIRI import plotting  # noqa F401
 
+# Set version
+__version__ = importlib.metadata.version('PyIRI')
+    
 # Determine the coefficient root directory
 try:
     coeff_dir = str(importlib.resources.files(__package__).joinpath(
