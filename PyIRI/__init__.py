45914ab2b1ca0378f4bc5d978f1e6e10e69b170b
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2023-08-15 19:08:41 -0400

    BUG: use explicit import
    Use explicit imports for importlib submodules.

--- PyIRI/__init__.py
+++ PyIRI/__init__.py
@@ -1,27 +1,30 @@
 """Core library imports for PyIRI."""
 
-import importlib
+try:
+    import importlib.metadata as metadata
+except ImportError:
+    import importlib_metadata as metadata
+
+try:
+    import importlib.resources as resources
+except ImportError:
+    import os
+    resources = None
 
 from PyIRI import igrf_library  # noqa F401
 from PyIRI import main_library  # noqa F401
 from PyIRI import plotting  # noqa F401
 
 # Set version
-try:
-    __version__ = importlib.metadata.version('PyIRI')
-except AttributeError:
-    import importlib_metadata
-    __version__ = importlib_metadata.version('PyIRI')
-    del importlib_metadata
+__version__ = metadata.version('PyIRI')
 
 # Determine the coefficient root directory
-try:
-    coeff_dir = str(importlib.resources.files(__package__).joinpath(
-        'coefficients'))
-except AttributeError:
-    import os
+if resources is None:
     coeff_dir = os.path.join(os.path.realpath(os.path.dirname(__file__)),
                              'coefficients')
     del os
+else:
+    coeff_dir = str(importlib.resources.files(__package__).joinpath(
+        'coefficients'))
 
-del importlib
+del metadata, resources
