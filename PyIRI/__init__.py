c75a8a85535e3acaceb4854aaf76887d9acc99ea
Angeline G. Burrell <aburrell@users.noreply.github.com>
2026-08-14 10:43:34 -0400

    MAINT: removed support for Python 3.9
    As Python 3.9 is no longer supported, remove the lines needed to get around the changes to importlib.

--- PyIRI/__init__.py
+++ PyIRI/__init__.py
@@ -1,18 +1,13 @@
 """Core library imports for PyIRI."""
 
-# Define a logger object to allow easier log handling
+from importlib import metadata
+from importlib import resources
 import logging
+
+# Define a logger object to allow easier log handling
 logging.raiseExceptions = False
 logger = logging.getLogger('pyiri_logger')
 
-osflag = False
-try:
-    from importlib import metadata
-    from importlib import resources
-except ImportError:
-    import importlib_metadata as metadata
-    import os
-    osflag = True
 
 # Import the package modules and top-level classes
 from PyIRI import edp_update  # noqa F401
@@ -24,10 +19,4 @@ from PyIRI import plotting  # noqa F401
 __version__ = metadata.version('PyIRI')
 
 # Determine the coefficient root directory
-if osflag:
-    coeff_dir = os.path.join(os.path.realpath(os.path.dirname(__file__)),
-                             'coefficients')
-else:
-    coeff_dir = str(resources.files(__package__).joinpath('coefficients'))
-
-del osflag
+coeff_dir = str(resources.files(__package__).joinpath('coefficients'))
